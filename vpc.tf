9d26ac6baa03fa7d1f49c1bb23817172cf267429
Hamid Raza <hrazaf6@gmail.com>
2020-02-06 21:06:05 +0530

    added the =

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "default" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  tags {
+  tags = {
     Name            = format("%s-vpc", var.env_name)
     ManagedBy       = var.manageby
     EnvironmentName = var.env_name
